08af4a6d072ba9c0b036eeb705fb7161bb15b20b
AhsanPhero2022e <whitehat2090@gmaile.com>
2023-05-08 11:36:50 +0600

    responsive sliding area

--- src/components/Home/CardSlider.jsx
+++ src/components/Home/CardSlider.jsx
@@ -26,121 +26,121 @@ const CardSlider = () => {
         Course Video
       </h2>
 
-      <Slider className="mx-8 lg:mx-24 text-center " {...settings}>
-        <div>
+      <Slider className="mx-8  text-center " {...settings}>
+        <div className="p-2">
           <ReactPlayer
             controls
-            url="https://youtu.be/AzUnRbv3SYI?list=PLPzr3Bjri7RmzcoXI5fxtmketjOGbyF-5"
-            width="350px"
-            height="300px"
+            url="https://youtu.be/-zj1JPQcLpg"
+            width="100%"
+            height=" 350px"
           />
           <Link target="_blank" to="https://www.youtube.com/@Ratulphysics">
-            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 w-1/2 px-5 py-2 rounded-md">
-              Go Youtube
+            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 mx-auto   px-5 py-2 rounded-md">
+              Go to Youtube
             </h2>
           </Link>
         </div>
-        <div>
+        <div className="p-2 ">
           <ReactPlayer
             controls
             url="https://youtu.be/AzUnRbv3SYI?list=PLPzr3Bjri7RmzcoXI5fxtmketjOGbyF-5"
-            width="350px"
-            height="300px"
+            width="100%"
+            height=" 350px"
           />
           <Link target="_blank" to="https://www.youtube.com/@Ratulphysics">
-            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 w-1/2 px-5 py-2 rounded-md">
-              Go Youtube
+            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2   px-5 py-2 rounded-md">
+              Go to Youtube
             </h2>
           </Link>
         </div>
-        <div>
+        <div className="p-2">
           <ReactPlayer
             controls
-            url="https://youtu.be/AzUnRbv3SYI?list=PLPzr3Bjri7RmzcoXI5fxtmketjOGbyF-5"
-            width="350px"
-            height="300px"
+            url="https://youtu.be/XjwlfKiX6AI"
+            width="100%"
+            height=" 350px"
           />
           <Link target="_blank" to="https://www.youtube.com/@Ratulphysics">
-            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 w-1/2 px-5 py-2 rounded-md">
-              Go Youtube
+            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2   px-5 py-2 rounded-md">
+              Go to Youtube
             </h2>
           </Link>
         </div>
-        <div>
+        <div className="p-2">
           <ReactPlayer
             controls
             url="https://youtu.be/AzUnRbv3SYI?list=PLPzr3Bjri7RmzcoXI5fxtmketjOGbyF-5"
-            width="350px"
-            height="300px"
+            width="100%"
+            height=" 350px"
           />
           <Link target="_blank" to="https://www.youtube.com/@Ratulphysics">
-            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 w-1/2 px-5 py-2 rounded-md">
-              Go Youtube
+            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2   px-5 py-2 rounded-md">
+              Go to Youtube
             </h2>
           </Link>
         </div>
-        <div>
+        <div className="p-2">
           <ReactPlayer
             controls
-            url="https://youtu.be/AzUnRbv3SYI?list=PLPzr3Bjri7RmzcoXI5fxtmketjOGbyF-5"
-            width="350px"
-            height="300px"
+            url="https://youtu.be/MBoc2TzCSYM"
+            width="100%"
+            height=" 350px"
           />
           <Link target="_blank" to="https://www.youtube.com/@Ratulphysics">
-            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 w-1/2 px-5 py-2 rounded-md">
-              Go Youtube
+            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2   px-5 py-2 rounded-md">
+              Go to Youtube
             </h2>
           </Link>
         </div>
-        <div>
+        <div className="p-2">
           <ReactPlayer
             controls
-            url="https://youtu.be/AzUnRbv3SYI?list=PLPzr3Bjri7RmzcoXI5fxtmketjOGbyF-5"
-            width="350px"
-            height="300px"
+            url="https://youtu.be/MTwglTniINE"
+            width="100%"
+            height=" 350px"
           />
           <Link target="_blank" to="https://www.youtube.com/@Ratulphysics">
-            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 w-1/2 px-5 py-2 rounded-md">
-              Go Youtube
+            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2   px-5 py-2 rounded-md">
+              Go to Youtube
             </h2>
           </Link>
         </div>
-        <div>
+        <div className="p-2">
           <ReactPlayer
             controls
             url="https://youtu.be/AzUnRbv3SYI?list=PLPzr3Bjri7RmzcoXI5fxtmketjOGbyF-5"
-            width="350px"
-            height="300px"
+            width="100%"
+            height=" 350px"
           />
           <Link target="_blank" to="https://www.youtube.com/@Ratulphysics">
-            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 w-1/2 px-5 py-2 rounded-md">
-              Go Youtube
+            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2   px-5 py-2 rounded-md">
+              Go to Youtube
             </h2>
           </Link>
         </div>
-        <div>
+        <div className="p-2">
           <ReactPlayer
             controls
             url="https://youtu.be/AzUnRbv3SYI?list=PLPzr3Bjri7RmzcoXI5fxtmketjOGbyF-5"
-            width="350px"
-            height="300px"
+            width="100%"
+            height=" 350px"
           />
           <Link target="_blank" to="https://www.youtube.com/@Ratulphysics">
-            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 w-1/2 px-5 py-2 rounded-md">
-              Go Youtube
+            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2   px-5 py-2 rounded-md">
+              Go to Youtube
             </h2>
           </Link>
         </div>
-        <div>
+        <div className="p-2">
           <ReactPlayer
             controls
-            url="https://youtu.be/AzUnRbv3SYI?list=PLPzr3Bjri7RmzcoXI5fxtmketjOGbyF-5"
-            width="350px"
-            height="300px"
+            url="https://youtu.be/oqPA8apMUkw"
+            width="100%"
+            height=" 350px"
           />
           <Link target="_blank" to="https://www.youtube.com/@Ratulphysics">
-            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 w-1/2 px-5 py-2 rounded-md">
-              Go Youtube
+            <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2   px-5 py-2 rounded-md">
+              Go to Youtube
             </h2>
           </Link>
         </div>
--- src/components/YouTube.jsx
+++ src/components/YouTube.jsx
@@ -33,89 +33,86 @@ const YouTube = () => {
 
   return (
     <div className="bg-gradient-to-r from-blue-200 px-6 to-blue-400 min-h-max  ">
-      <div className=" lg:mx-16 py-12">
+      <div className="  py-12">
         <h2 className="text-2xl font-semibold text-center py-12 text-white">
           YouTube Video
         </h2>
-        <Slider className="" {...settings}>
-          <div>
+        <Slider {...settings}>
+          <div className="px-3">
             <ReactPlayer
-              volume={1}
-              muted={false}
-              playing={false}
               controls
               url="https://youtu.be/L6_8J-YqmJE"
-              width="300px"
-              height="300px"
+              width="100%"
+              height="350px"
             />
             <Link target="_blank" to="https://www.youtube.com/@Ratulphysics">
-              <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 w-1/2 px-5 py-2 rounded-md">
-                Go Youtube
+              <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2   py-2 rounded-md text-center">
+                Go to Youtube
               </h2>
             </Link>
           </div>
-          <div>
+          <div className="px-3">
             <ReactPlayer
               controls
               url="https://youtu.be/Q6upN0WgISs?list=PLPzr3Bjri7RkoA5VTNKrRy-FSZmonNbUj"
-              width="300px"
-              height="300px"
+              width="100%"
+              height="350px"
             />
             <Link target="_blank" to="https://www.youtube.com/@Ratulphysics">
-              <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 w-1/2 px-5 py-2 rounded-md">
-                Go Youtube
+              <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2   px-5 py-2 rounded-md text-center">
+                Go to Youtube
               </h2>
             </Link>
           </div>
-          <div>
+          <div className="px-3">
             <ReactPlayer
               controls
               url="https://youtu.be/i4bvgVKZgQ4"
-              width="300px"
-              height="300px"
+              width="100%"
+              height="350px"
             />
             <Link target="_blank" to="https://www.youtube.com/@Ratulphysics">
-              <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 w-1/2 px-5 py-2 rounded-md">
-                Go Youtube
+              <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2   px-5 py-2 rounded-md text-center">
+                Go to Youtube
               </h2>
             </Link>
           </div>
-          <div>
+          <div className="px-3">
             <ReactPlayer
               controls
               url="https://youtu.be/RA6ccYgN0AQ"
-              width="300px"
-              height="300px"
+              width="100%"
+              height="350px"
             />
             <Link target="_blank" to="https://www.youtube.com/@Ratulphysics">
-              <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 w-1/2 px-5 py-2 rounded-md">
-                Go Youtube
+              <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2   px-5 py-2 rounded-md text-center">
+                Go to Youtube
               </h2>
             </Link>
           </div>
-          <div>
+          <div className="px-3">
             <ReactPlayer
               controls
               url="https://youtu.be/pZoO3-2qfJw"
-              width="300px"
-              height="300px"
+              width="100%"
+              height="350px"
             />
             <Link target="_blank" to="https://www.youtube.com/@Ratulphysics">
-              <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 w-1/2 px-5 py-2 rounded-md">
-                Go Youtube
+              <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2   px-5 py-2 rounded-md text-center">
+                Go to Youtube
               </h2>
             </Link>
           </div>
-          <div>
+          <div className="px-3">
             <ReactPlayer
               controls
               url="https://youtu.be/AzUnRbv3SYI?list=PLPzr3Bjri7RmzcoXI5fxtmketjOGbyF-5"
-              width="300px"
-              height="300px"
+              width="100%"
+              height="350px"
             />
             <Link target="_blank" to="https://www.youtube.com/@Ratulphysics">
-              <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2 w-1/2 px-5 py-2 rounded-md">
-                Go Youtube
+              <h2 className="text-2xl font-semibold bg-slate-200 hover:bg-blue-400 my-2   px-5 py-2 rounded-md text-center">
+                Go to Youtube
               </h2>
             </Link>
           </div>
